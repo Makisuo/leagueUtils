16a4ca3f59f6b784f3c8bcc19e1cad9b41e43b3a
Makisuo <minetoaster12@gmail.com>
2020-06-17 23:59:42 +0200

    Some ChampPage updates

--- src/pages/ChampPage.js
+++ src/pages/ChampPage.js
@@ -1,8 +1,35 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Box, Container, Typography, Paper, Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 
-const UltimateBravery = () => {
-	return <Typography variant={'h4'}>Coming soon</Typography>
+const useStyles = makeStyles((theme) => ({
+	root: {
+		padding: theme.spacing(4),
+	},
+	paper: {
+		marginBottom: theme.spacing(1),
+		padding: theme.spacing(4),
+	},
+}))
+
+const ChampPage = (props) => {
+	const classes = useStyles()
+	const [ champion, setChampion ] = useState('Annie')
+
+	if (props.location.state && props.location.state.champion !== champion) {
+		setChampion(props.location.state.champion)
+	}
+
+	return (
+		<Container className={classes.root}>
+			<Paper className={classes.paper}>
+				<Avatar />
+				<Typography variant='h5' align='center'>
+					{champion}
+				</Typography>
+			</Paper>
+		</Container>
+	)
 }
 
-export default UltimateBravery
+export default ChampPage
